d31351cf1074d3b2edbb7c5fb2c99722387a3a64
Shreck Ye <ShreckYe@gmail.com>
2024-09-30 16:15:49 +0800

    Update the project name to "codeforces-kotlin"

--- kotlin/settings.gradle.kts
+++ kotlin/settings.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
-rootProject.name = "codeforces"
+rootProject.name = "codeforces-kotlin"
 
 include("kotlin-heroes:practice-11")
 include("test")
