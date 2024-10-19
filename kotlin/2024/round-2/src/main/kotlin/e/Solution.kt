5211068a09ffdc9153e631b03e065e607ea4330b
Shreck Ye <ShreckYe@gmail.com>
2024-10-20 00:21:14 +0800

    Create a project for 2024 Round 2 and copy the template code

---
+++ kotlin/2024/round-2/src/main/kotlin/e/Solution.kt
@@ -0,0 +1,17 @@
+package e
+
+fun main() {
+    val tt = readln().toInt()
+    repeat(tt, ::testCase)
+}
+
+fun testCase(tti: Int) {
+    TODO("read input")
+    //val _ = readln().toInt()/.toLong()
+    //val _ = readln().splitToSequence(' ').map { it.toInt()/.toLong() }.toList()
+
+    TODO("compute")
+
+    val y = TODO()
+    println("Case #${tti + 1}: $y")
+}
